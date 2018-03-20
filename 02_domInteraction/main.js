d147b36c28ae22bc7163edcd9ec7a0aa690a8196
jendiamond <thejendiamond@gmail.com>
2018-03-20 14:55:02 -0700

    Add v-once && raw HTML output
    + Video 13 - Understanding and using Directives
    + Video 14 - Disable re-rendering with v-once
    + Video 15 - How to output Raw HTMLB
    
    Branch 2_domInteraction
    
    Changes to be committed:
    + modified:   02_domInteraction/index.html
    + modified:   02_domInteraction/main.js

--- 02_domInteraction/main.js
+++ 02_domInteraction/main.js
@@ -3,6 +3,7 @@ new Vue({
   data: {
     title: 'Hello World',
     link: 'http://google.com/',
+    finishedLink: "<a href='http://google.com/'>Google</a>",
   },
   methods: {
     getTitle: function() {
