49d7869dbc229ff7173b67c7412fe71958e63547
jendiamond <thejendiamond@gmail.com>
2018-03-20 17:35:28 -0700

    Complete Assignment 1
    1. Fill the <p> below with your Name and Age - using Interpolation -->
    2. Output your age, multiplied by 3 -->
    3. Call a function to output a random float between 0 and 1
    (Math.random()) -->
    4. Search any image on Google and output it here by binding the "src"
    attribute -->
    5. Pre-Populate this input with your name (set the "value" attribute)
    
    ---
    
    On branch 2_domInteraction
    
    Changes to be committed:
    + new file:   02_domInteraction/Assignment1/README.md
    + modified:   02_domInteraction/Assignment1/index.html
    + modified:   02_domInteraction/Assignment1/main.js

---
+++ 02_domInteraction/Assignment1/README.md
@@ -0,0 +1,10 @@
+### Fill the <p> below with your Name and Age using Interpolation
+`<p>{{ name }} {{ age }}</p>`
+
+### Output your age, multiplied by 3
+`<p></p>`
+
+### Call a function to output a random float between 0 and 1 (Math.random())
+`<p></p>`
+
+### Search any image on Google and output it here by binding the "src" attribute
--- 02_domInteraction/Assignment1/main.js
+++ 02_domInteraction/Assignment1/main.js
@@ -1,11 +1,25 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello World'
+    name: 'Jen Diamond',
+    age: 33,
+    dataAndMethodsLink: "<a href ='https://youtu.be/2MAoq2-2nnE'>Data and Methods Tutorial</a>",
+    peep: 'https://i.pinimg.com/736x/8c/50/4c/8c504c6758edfab2e84fa1fbf129ba31--peep-show-happy-happy-happy.jpg',
   },
   methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
-    }
+    multipyByThree: function(num) {
+      return parseInt(num) * 3;
+    },
+    getRandomNum: function (min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max)+ 1;
+      return Math.floor(Math.random() * (max - min)) + min;
+    },
+    changeName: function(argument) {
+      this.name = event.target.value;
+    },
+    changeAge: function(argument) {
+      this.age = event.target.value;
+    },
   }
 });
