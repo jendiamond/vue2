e66d5ebe71a66bd09e0a991b10cbd612ed799474
jendiamond <thejendiamond@gmail.com>
2018-04-20 22:18:06 -0700

    Add Attack method
    ---
    
    On branch 04_monsterSlayer_AttackMethod
    
    Changes to be committed:
    modified:   04_monsterSlayer/monsterSlayer_Videos_46-61/index.html
    modified:   04_monsterSlayer/monsterSlayer_Videos_46-61/main.js

--- 04_monsterSlayer/monsterSlayer_Videos_46-61/main.js
+++ 04_monsterSlayer/monsterSlayer_Videos_46-61/main.js
@@ -6,10 +6,44 @@ new Vue({
     monsterHealth: 100,
   },
   methods: {
-    startGame: function(){
+    startGame(){
       this.gameOn = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-    }
+    },
+    attack() {
+      this.playerHealth -= this.calculateDamage(4, 12);
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      this.checkForWin();
+    },
+    getRandom: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+      //Both the maximum and the minimum are inclusive
+    },
+    calculateDamage: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    checkForWin() {
+      if (this.monsterHealth <= 0){
+        if (confirm("YOU DESTROYED THE MONSTER!  :0")){
+          this.startGame();
+        } else {
+            this.gameOn = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0){
+        if (confirm("THE MONSTER ATE YOU!  :(")){
+          this.startGame();
+        } else {
+        this.gameOn = false;
+        }
+        return true;
+      }
+      return false;
+    },
   }
 });
