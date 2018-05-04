dcb36fbfb1814d39623b009ce775ec9dbfe4ca99
Jen Diamond <thejendiamond@gmail.com>
2018-05-03 20:38:27 -0700

    Initial commit

---
+++ 07_components/Assignment6/src/components/Servers.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <div class='top'>
+    <ul>
+      <li
+        class='list-group-item'
+        v-for='index in 5'>
+        Server Number {{ index }}
+      </li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .list-group-item {
+    background: goldenrod;
+    border: 1px solid #ccc;
+    margin-top: 10px;
+    color: white;
+    font-size: 1.5em;
+  }
+</style>
