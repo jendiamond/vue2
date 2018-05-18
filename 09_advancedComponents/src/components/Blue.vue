ea368732238e7742479a640509de0016b90066cb
Jen Diamond <thejendiamond@gmail.com>
2018-05-17 17:47:56 -0700

    Reformat white spacing

--- 09_advancedComponents/src/components/Blue.vue
+++ 09_advancedComponents/src/components/Blue.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+  <div></div>
 </template>
 
 <script>
@@ -7,11 +7,11 @@
 </script>
 
 <style scoped>
-    div {
-        border: 1px solid blue;
-        background-color: lightblue;
-        padding: 30px;
-        margin: 20px auto;
-        text-align: center
-    }
+  div {
+    border: 1px solid blue;
+    background-color: lightblue;
+    padding: 30px;
+    margin: 20px auto;
+    text-align: center
+  }
 </style>
